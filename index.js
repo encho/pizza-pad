893aedcb3a230fda3c26b85bb02ca23131a72537
Lorenzo Bertolini <sig.lorenzo.bertolini@gmail.com>
2016-11-30 10:59:32 +0100

    log out pad keys

--- index.js
+++ index.js
@@ -2,10 +2,23 @@
 const Launchpad = require( 'launchpad-mini' );
 const pad = new Launchpad();
 
+// possible launchpad mini button states
+// --------------------------------
+// pad.red
+// pad.green
+// pad.amber
+// pad.off
+// pad.yellow
+
 pad.connect().then( () => {     // Auto-detect Launchpad
     pad.reset( 2 );             // Make Launchpad glow yellow
-    pad.on( 'key', k => {
-        // pad.col( pad.red, k );  // Turn on buttons on press
-        pad.col( pad.green, k );  // Turn on buttons on press
+    pad.on( 'key', key => {
+      const { x, y, pressed } = key;
+      console.log('key pressed:');
+      console.log('------------');
+      console.log(x);
+      console.log(y);
+      console.log(pressed);
+      pad.col( pad.green, key );  // Turn on buttons on press
     } );
 } );
